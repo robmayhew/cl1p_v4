04ae2fdf2580f04b524e2720f557b29284e1065a
Rob Mayhew <rob@robmayhew.com>
2024-05-11 07:25:57 -0600

    First models in django prototype. Started to track names of things.

--- django_prototype/cl1pv4/cl1papp/models.py
+++ django_prototype/cl1pv4/cl1papp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class RMPathKey(models.Model):
+    id = models.AutoField(primary_key=True)
+    path = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.path
+    
+class RMPathContent(models.Model):
+    id = models.AutoField(primary_key=True)
+    key = models.ForeignKey(RMPathKey, on_delete=models.CASCADE)
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.key.path
--- django_prototype/cl1pv4/cl1papp/views.py
+++ django_prototype/cl1pv4/cl1papp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import RMPathKey
 
 # Create your views here.
 def wildcard_view(request, path):
-    return render(request, 'cl1papp/cl1p_found.html', {'path': path})
+    rm_path = get_cl1p_instance_by_path(path)
+    if rm_path is None:
+        rm_path = RMPathKey()
+        rm_path.path = path
+        rm_path.save()
+        return render(request, 'cl1papp/cl1p_not_found.html', {'path': path})
+    else:
+        rm_path.delete()
+        return render(request, 'cl1papp/cl1p_found.html', {'path': path})
+
+
+
+def get_cl1p_instance_by_path(path):
+    try:
+        return RMPathKey.objects.get(path=path)
+    except RMPathKey.DoesNotExist:
+        return None
