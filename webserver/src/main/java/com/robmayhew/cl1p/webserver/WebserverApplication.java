de308117bbc8051263601cc922af9d0fd725de01
Rob Mayhew <rob@robmayhew.com>
2024-08-16 14:05:07 -0600

    Initial Spring Boot

---
+++ webserver/src/main/java/com/robmayhew/cl1p/webserver/WebserverApplication.java
@@ -0,0 +1,13 @@
+package com.robmayhew.cl1p.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserverApplication.class, args);
+    }
+
+}
---
+++ webserver/src/test/java/com/robmayhew/cl1p/webserver/WebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robmayhew.cl1p.webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
